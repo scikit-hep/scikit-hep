39d420e22616eccdc257e4d5d22912397ad1a1fb
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2016-12-28 16:37:59 +0100

    Added subpackage constants/. Just a few definitions for now.

---
+++ skhep/constants/__init__.py
@@ -0,0 +1,16 @@
+# Licensed under a 3-clause BSD style license, see LICENSE.
+"""
+************************
+Subpackage for constants
+************************
+
+  * Physical constants.
+  * Common/handy constants.
+"""
+
+#-----------------------------------------------------------------------------
+# Import statements
+#-----------------------------------------------------------------------------
+from __future__ absolute_import
+
+from .constants import *
---
+++ skhep/constants/constants.py
@@ -0,0 +1,33 @@
+# Licensed under a 3-clause BSD style license, see LICENSE.
+"""
+Physical and other handy constants
+==================================
+
+All constants are computed in the HEP System of Units
+as defined in the units module.
+"""
+
+#-----------------------------------------------------------------------------
+# Import statements
+#----------------------------------------------------------------------------- 
+from __future__ import absolute_import
+
+from math import pi
+
+from ..units import *
+
+#-----------------------------------------------------------------------------
+# Mathematical constants
+#-----------------------------------------------------------------------------
+
+twopi  = 2 * pi
+halfpi = pi / 2
+pi2    = pi * pi
+
+#-----------------------------------------------------------------------------
+# Physical constants
+#-----------------------------------------------------------------------------
+
+# Speed of light in vacuum
+c_light   = 299792458 * m/s
+c_squared = c_light * c_light
