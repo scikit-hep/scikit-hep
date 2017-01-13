c24b69ad7c2010106fd5dcec4b07ebd389d0b243
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2017-01-13 11:51:08 +0100

    Banner improved.

--- skhep/__init__.py
+++ skhep/__init__.py
@@ -1,8 +1,10 @@
 # Licensed under a 3-clause BSD style license, see LICENSE.
 """
-___  __ .    . ___       ___ ___
-|_  |   | |/ |  |  _ |_| |_  |_|
-__| |__ | |\ |  |    | | |__ |     {0}
+  .-.          .         .        .   ..---..--. 
+ (   )      o  |      o _|_       |   ||    |   )
+  `-.  .-.  .  |.-.   .  |   ____ |---||--- |--' 
+ (   )(     |  |-.'   |  |        |   ||    |    
+  `-'  `-'-' `-'  `--' `-`-'      '   ''---''      {0}
 
 A community-driven and oriented Python software project for High Energy Physics.
 """
