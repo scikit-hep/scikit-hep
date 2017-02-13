d3181c8b7da4ce0933f67338bc3bd3b76e516fea
Alex Pearce <alex@alexpearce.me>
2017-02-13 16:53:30 +0100

    Fix docstrings like break the Sphinx build.

--- skhep/constants/__init__.py
+++ skhep/constants/__init__.py
@@ -9,14 +9,14 @@ Subpackage for constants
 
 Typical use case::
 
->>> from skhep.constants import c_light
->>> from skhep.units     import picosecond, micrometer
->>> tau_Bs = 1.5 * picosecond    # a particle lifetime, say the Bs meson's
->>> ctau_Bs = c_light * tau_Bs   # ctau of the particle, roughly 450 microns
->>> print ctau_Bs   # result in HEP units, so mm ;-)
-0.449688687
->>> print ctau_Bs / micrometer   # result in micrometers
-449.688687
+    >>> from skhep.constants import c_light
+    >>> from skhep.units     import picosecond, micrometer
+    >>> tau_Bs = 1.5 * picosecond    # a particle lifetime, say the Bs meson's
+    >>> ctau_Bs = c_light * tau_Bs   # ctau of the particle, roughly 450 microns
+    >>> print ctau_Bs   # result in HEP units, so mm ;-)
+    0.449688687
+    >>> print ctau_Bs / micrometer   # result in micrometers
+    449.688687
 
 """
 
