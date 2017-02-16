c038ab37e5ccce0a446e2f855abb612c5df5100f
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2017-02-16 11:19:46 +0100

    Update rootdataset.py

--- skhep/dataset/rootdataset.py
+++ skhep/dataset/rootdataset.py
@@ -1,8 +1,8 @@
 # Licensed under a 3-clause BSD style license, see LICENSE.
 """
-***********************
-Module for NumpyDataset
-***********************
+**********************
+Module for ROOTDataset
+**********************
 """
 
 #-----------------------------------------------------------------------------
