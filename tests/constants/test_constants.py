838e2e224b7e516f6bf0502704e4324e45a5d8d1
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2017-05-02 16:28:45 +0200

    Extra tests of units and constants.

--- tests/constants/test_constants.py
+++ tests/constants/test_constants.py
@@ -20,5 +20,7 @@ class Test(unittest.TestCase):
         self.test_constants()
 
     def test_constants(self):
+        self.assertEqual( pi_sq, two_pi * half_pi )
+        self.assertEqual( Avogadro, 6.022140857e+23 )
         self.assertEqual( c_light / (m/s), 299792458 )
         self.assertAlmostEqual( hbarc_sq / c_light_sq, (h_Planck/two_pi)**2 )
