80dc0ab963f61508a5db893d5c28bcd664420a4c
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2017-09-28 16:29:51 +0200

    Added tests for the particle data table methods

--- tests/pdg/test_pdt.py
+++ tests/pdg/test_pdt.py
@@ -6,7 +6,7 @@
 #-----------------------------------------------------------------------------
 import unittest
 
-from skhep.math import *
+from skhep.pdg import *
 from skhep.utils.py23 import *
 
 #-----------------------------------------------------------------------------
@@ -18,4 +18,9 @@ class Test(unittest.TestCase):
         self.test_pdt()
 
     def test_pdt(self):
-        pass
+        tbl = ParticleDataTable('skhep/data/mass_width_2016.mcd')
+        self.assertEqual(tbl('f(0)(500)0').name,tbl[9000221].name)
+        self.assertEqual(tbl('f(0)(500)0').id,9000221)
+        self.assertTrue(tbl.has_key(11))
+        self.assertTrue(tbl.has_particle(2212))
+        self.assertEqual(tbl.particle(21).__repr__(),'g0: ID=21, m=0 GeV, 3*q=0, width=0 GeV')
