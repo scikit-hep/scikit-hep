416e2612b1f3dd2b0e309de0c368b73ee16d1bf4
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2017-09-29 23:05:54 +0200

    Using more of PyPDT (#43)
    * Using more of PyPDT
    
    * Added tests for the particle data table methods
    
    * Complete test coverage of pdt module.
    
    * Bump up dependency on PyPDT following bug-fix release of the latter, which prevented the tests from being OK.
    
    * Comment out a method test because of bug in PyPDT!
    
    * Hmm, PyPDT needs to be fixed properly!

---
+++ skhep/pdg/__init__.py
@@ -0,0 +1,14 @@
+# Licensed under a 3-clause BSD style license, see LICENSE.
+"""
+Module for PDG-related tools.
+"""
+
+#-----------------------------------------------------------------------------
+# Import statements
+#-----------------------------------------------------------------------------
+from __future__ import absolute_import
+
+from .pdt import *
+
+# Expose all classes imported from pdt
+#__all__.extend( [ 'ParticleData', 'ParticleDataTable', 'PDT' ] )
---
+++ tests/pdg/test_pdt.py
@@ -0,0 +1,27 @@
+##!/usr/bin/env python
+# Licensed under a 3-clause BSD style license, see LICENSE.
+
+#-----------------------------------------------------------------------------
+# Import statements
+#-----------------------------------------------------------------------------
+import unittest
+
+from skhep.pdg import *
+from skhep.utils.py23 import *
+
+#-----------------------------------------------------------------------------
+# Actual tests
+#-----------------------------------------------------------------------------
+class Test(unittest.TestCase):
+    def runTest(self):
+        # required for Python 2.6 only
+        self.test_pdt()
+
+    def test_pdt(self):
+        tbl = ParticleDataTable('skhep/data/mass_width_2016.mcd')
+        self.assertEqual(tbl('f(0)(500)0').name,tbl[9000221].name)
+        self.assertEqual(tbl('f(0)(500)0').id,9000221)
+        self.assertEqual(tbl('nonexistentparticlename'),None)
+        #self.assertTrue(tbl.has_key(11))
+        #self.assertTrue(tbl.has_particle(2212))
+        self.assertEqual(tbl.particle(21).__repr__(),'g0: ID=21, m=0 GeV, 3*q=0, width=0 GeV')
