0617d5692d8067161e1c5a3810f6aaab7718d9a8
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2020-01-15 17:38:43 +0100

    Updates towards 1st release as metapackage (#103)
    * Update CI to test Python 3.7
    
    * LICENSE date update
    
    * Dependencies update to latest releases of several packages
    
    * Cleanup and update travis
    
    Co-authored-by: Henry Schreiner <HenrySchreinerIII@gmail.com>

--- setup.py
+++ setup.py
@@ -13,23 +13,12 @@ from setuputils import read, find_version
 PROJECT_NAME = 'scikit-hep'
 AUTHOR = 'the scikit-hep developers'
 AUTHOR_EMAIL = 'scikit-hep-admins@googlegroups.com'
-DESCRIPTION = 'Toolset of interfaces and tools for Particle Physics.'
+DESCRIPTION = 'Metapackage of Scikit-HEP project tools for Particle Physics.'
 URL = 'https://github.com/scikit-hep/scikit-hep/'
 LICENSE = 'new BSD'
 VERSION = find_version('skhep/__init__.py')
 
-# Prevent setup from trying to create hard links
-# which are not allowed on AFS between directories.
-# This is a hack to force copying.
-try:
-    del os.link
-except AttributeError:
-    pass
-
 LOCAL_PATH = os.path.dirname(os.path.abspath(__file__))
-# setup.py can be called from outside the scikit-hep directory
-os.chdir(LOCAL_PATH)
-sys.path.insert(0, LOCAL_PATH)
 
 PYTHON_REQUIRES = ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*"
 
@@ -38,10 +27,6 @@ with open(os.path.join(LOCAL_PATH, 'requirements.txt')) as requirements_file:
 
 TESTS_REQUIRE = [
     'pytest>3.0;python_version>="2.7"'
-]
-
-SETUP_REQUIRES = [
-    'pytest-runner'
 ]
 
 setup(
@@ -58,7 +43,6 @@ setup(
     python_requires=PYTHON_REQUIRES,
     install_requires=INSTALL_REQUIRES,
     tests_require=TESTS_REQUIRE,
-    setup_requires=SETUP_REQUIRES,
     classifiers=[
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
