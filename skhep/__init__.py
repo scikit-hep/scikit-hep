275d0ccf797183ac8d7c0d0790d4948710572e08
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2025-01-14 15:25:24 +0100

    -Trivial fix

--- skhep/__init__.py
+++ skhep/__init__.py
@@ -15,7 +15,8 @@ __all__ = [
     "banner",
     "project_url",
     "project_url_GitHub",
-    "project_url_PyPIshow_versions",
+    "project_url_PyPI",
+    "show_versions",
     "__version__",
 ]
 
